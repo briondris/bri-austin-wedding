2c96035ca5862215c48cd4dd7fcd400f6a1addc9
Brianna <briannaondris@brightlineinteractive.com>
2026-08-01 22:01:15 -0700

    text update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,8 +25,7 @@ export default function Home() {
                 <br /> then the rest is history.&quot;
               </p>
               <p className="text-xs sm:text-sm text-white/90 drop-shadow-md mt-2">
-                — CARRTOONS, &quot;Labor Of Love&quot; — CARRTOONS, &quot;Labor
-                Of Love&quot;
+                — CARRTOONS, &quot;Labor Of Love&quot;
               </p>
             </div>
 
@@ -85,12 +84,7 @@ export default function Home() {
               Shenandoah, and went to our first concert as a couple (Washed
               Out). We became best travel buddies, then each other&apos;s rocks.
               One cross-country move to San Francisco later, here we are,
-              happily engaged and excited to celebrate with you all! From there,
-              together we shared movies, like Lady Bird, hiked Shenandoah, and
-              went to our first concert as a couple (Washed Out). We became best
-              travel buddies, then each other&apos;s rocks. One cross-country
-              move to San Francisco later, here we are, happily engaged and
-              excited to celebrate with you all!
+              happily engaged and excited to celebrate with you all!
             </p>
             <Link
               href="/savethedate"
@@ -244,21 +238,16 @@ export default function Home() {
             </h2>
             <p className="text-base md:text-2xl text-cream/90 leading-relaxed">
               We had it all planned out, well, one of us did. Golden Gate Park
-              We had it all planned out, well, one of us did. Golden Gate Park
               is basically our backyard, so a walk there didn&apos;t raise any
               alarms, even though Bri knew something was going on. What she
               didn&apos;t know was that her sister cousin were already there,
               hidden nearby, with Hunter (cousin-in-law) waiting to catch the
-              moment on camera the second we showed up. didn&apos;t know was
-              that her sister cousin were already there, hidden nearby, with
-              Hunter (cousin-in-law) waiting to catch the moment on camera the
-              second we showed up.
+              moment on camera the second we showed up.
             </p>
             <p className="text-base md:text-2xl text-cream/90 leading-relaxed mb-8">
               It was quiet, it was ours, and nobody even walked past. We went
               from that little patch of woods to planning a wedding in the woods
-              of Michigan, turns out we really do love the trees. of Michigan,
-              turns out we really do love the trees.
+              of Michigan, turns out we really do love the trees.
             </p>
           </div>
         </div>
